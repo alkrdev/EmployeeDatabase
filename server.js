5d83896be7286775b86c0c337511551039fada54
Alexander Kristensen <ak@kk-partner.dk>
2020-03-21 22:59:56 +0100

    Small correction

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors())
+
 const connection = mysql.createConnection({
     host: "HOST",
     user: "USER",
@@ -13,10 +15,8 @@ const connection = mysql.createConnection({
 })
 
 connection.connect()
-app.use(cors())
 
 require('./routes/routes.js')(app, connection);
-
 
 app.listen(PORT, () => {
     console.log(`SERVER RUNNING ON ${PORT}`)
--- showcase/main.js
+++ showcase/main.js
@@ -1,23 +1,29 @@
 let url = "http://localhost:3000"
 
 const request = async () => {
-    let response = await fetch(url, {mode: 'cors'});
-    console.log(response)
+    let response = await fetch(url, {
+        mode: 'cors',
+        method: 'GET',
+    });
+
     if (response.ok) { 
         let json = await response.json();
-        console.log(json)
-        for (let person of json.users) {
-            var table = document.getElementById("tableBody");
-
-            // Create an empty <tr> element and add it to the 1st position of the table:
-            var row = table.insertRow(-1);
-
-            // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-            row.insertCell(0).innerHTML = person.id;
-            row.insertCell(1).innerHTML = person.navn;
-        }
+        fillTable(json)
     } else {
         alert("HTTP-Error: " + response.status);
+    }
+}
+
+function fillTable(res) {
+    for (let person of res.users) {
+        var table = document.getElementById("tableBody");
+
+        // Create an empty <tr> element and add it to the 1st position of the table:
+        var row = table.insertRow(-1);
+
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        row.insertCell(0).innerHTML = person.id;
+        row.insertCell(1).innerHTML = person.navn;
     }
 }
 
